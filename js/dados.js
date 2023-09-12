6b651aa29f657e1c553fa595ce1c9b74fced1b60
LucasBS10 <lucasbernardo2604@gmail.com>
2023-09-12 09:02:22 -0300

    Site para loja fitness

---
+++ js/dados.js
@@ -0,0 +1,71 @@
+var MENU = {
+    "macacao": [
+        {
+            "id": "macacao-lilas",
+            "img": "./img/roupas/macacao/4.jpeg",
+            "name": "Macacão lilas",
+            "dsc": "Tecido poliéster sem bojo Empina bumbum veste do 36 ao 42",
+            "price": 50.00
+        },
+        {
+            "id": "macacao-rosa-preto",
+            "img": "./img/roupas/macacao/5.jpeg",
+            "name": "Macacão Rosa com Preto",
+            "dsc": "Macacão no tecido poliéster sem bojo",
+            "price": 50.00
+        },
+        {
+            "id": "macacao-cirre",
+            "img": "./img/roupas/macacao/6.jpeg",
+            "name": "Macacão cirrê",
+            "dsc": "Macaquinho no tecido cirrê poliamida com bojo",
+            "price": 70.00
+        }
+    ],
+    "calca": [
+        {
+            "id": "calca-sem-costura",
+            "img": "./img/roupas/calca/15.jpeg",
+            "name": "Calça sem costura",
+            "dsc": "Calça sem costura, veste 36 ao 44",
+            "price": 60.00
+        },
+        {
+            "id": "calca-poliamida",
+            "img": "./img/roupas/calca/21.jpeg",
+            "name": "Calça poliamida",
+            "dsc": "Calça no tecido poliamida empina bumbum sem costura veste 36 ao 44",
+            "price": 85.00
+        },
+        {
+            "id": "calca-estanpada",
+            "img": "./img/roupas/calca/30.jpeg",
+            "name": "Calça estanpada",
+            "dsc": "Calça no tecido poliamida veste 36 ao 42",
+            "price": 60.00
+        }
+    ],
+    "conjunto": [
+        {
+            "id": "conjunto-poliamida",
+            "img": "./img/roupas/conjunto/1.jpeg",
+            "name": "Conjunto poliamida",
+            "dsc": "Conjunto no tecido poliamida com bojo short duplo no M e G",
+            "price": 65.00
+        },
+        {
+            "id": "conjunto-jaguar",
+            "img": "./img/roupas/conjunto/9.jpeg",
+            "name": "Conjunto jaguar",
+            "dsc": "Conjunto no tecido poliamida jaquar empina bumbum com bojo veste do 36 ao 42",
+            "price": 80.00
+        },
+        {
+            "id": "conjunto-sem-costura",
+            "img": "./img/roupas/conjunto/14.jpeg",
+            "name": "Conjunto sem costura",
+            "dsc": "Conjunto sem costura veste 36 ao 44",
+            "price": 60.00
+        }
+    ]
+}
